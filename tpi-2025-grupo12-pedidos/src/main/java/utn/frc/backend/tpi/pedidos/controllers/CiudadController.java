3bcb3a4b591a2f448a00e7122af068e216ed9351
Nicolas Pereira <nicopereira2802@gmail.com>
2025-07-09 16:53:54 -0300

    CRUD ciudades

---
+++ tpi-2025-grupo12-pedidos/src/main/java/utn/frc/backend/tpi/pedidos/controllers/CiudadController.java
@@ -0,0 +1,50 @@
+package utn.frc.backend.tpi.pedidos.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import utn.frc.backend.tpi.pedidos.models.Ciudad;
+import utn.frc.backend.tpi.pedidos.services.CiudadService;
+
+@RestController
+@RequestMapping("/api/pedidos/ciudades")
+public class CiudadController {
+    
+    @Autowired
+    private CiudadService  ciudadServicio;
+
+    
+    @GetMapping
+    public List<Ciudad> listar(){
+        return ciudadServicio.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Ciudad listarPorId(@PathVariable Long id){
+        return ciudadServicio.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public Ciudad crear(@RequestBody Ciudad ciudad){
+        return ciudadServicio.crear(ciudad);
+    }
+
+    @PutMapping("/{id}")
+    public Ciudad actualizar(@PathVariable Long id, @RequestBody Ciudad ciudad){
+        return ciudadServicio.actualizar(id, ciudad);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id){
+        ciudadServicio.eliminar(id);
+    }
+}
