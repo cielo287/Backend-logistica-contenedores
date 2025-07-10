382f6eeff64e16466092e1b2fbac17dad76c71d0
Cielo Bertoni <cielobertoni@gmail.com>
2025-07-10 17:44:57 -0300

    DTOs

---
+++ tpi-2025-grupo12-pedidos/src/main/java/utn/frc/backend/tpi/pedidos/dto/CamionDTO.java
@@ -0,0 +1,25 @@
+package utn.frc.backend.tpi.pedidos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import utn.frc.backend.tpi.pedidos.models.Camion;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CamionDTO {
+    private Long id;
+    private double capacidadPeso;
+    private boolean disponibilidad;
+    private double volumen;
+
+    public CamionDTO(Camion camion) {
+        this.id = camion.getId();
+        this.capacidadPeso = camion.getCapacidadPeso();
+        this.volumen = camion.getVolumen();
+    }
+
+
+}
---
+++ tpi-2025-grupo12-pedidos/src/main/java/utn/frc/backend/tpi/pedidos/dto/ContenedorDTO.java
@@ -0,0 +1,5 @@
+package utn.frc.backend.tpi.pedidos.dto;
+
+public class ContenedorDTO {
+
+}
