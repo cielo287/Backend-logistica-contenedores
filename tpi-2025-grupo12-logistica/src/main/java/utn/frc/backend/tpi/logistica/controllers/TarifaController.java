07dd1571020924dc5cac9ba6c079d2a069497c7e
ldalmagro1 <94366@sistemas.frc.utn.edu.ar>
2025-07-10 11:04:49 -0300

    controladores de logistica creados

--- tpi-2025-grupo12-logistica/src/main/java/utn/frc/backend/tpi/logistica/controllers/TarifaController.java
+++ tpi-2025-grupo12-logistica/src/main/java/utn/frc/backend/tpi/logistica/controllers/TarifaController.java
@@ -1,5 +1,42 @@
 package utn.frc.backend.tpi.logistica.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import utn.frc.backend.tpi.logistica.models.Tarifa;
+import utn.frc.backend.tpi.logistica.services.TarifaService;
+
+@RestController
+@RequestMapping("/api/logistica/tarifas")
 public class TarifaController {
-    
+
+    @Autowired
+    private TarifaService tarifaService;
+
+    @GetMapping
+    public List<Tarifa> listar() {
+        return tarifaService.obtenerTodas();
+    }
+
+    @GetMapping("/{id}")
+    public Tarifa listarPorId(@PathVariable Long id) {
+        return tarifaService.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public Tarifa crear(@RequestBody Tarifa tarifa) {
+        return tarifaService.crear(tarifa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarifa actualizar(@PathVariable Long id, @RequestBody Tarifa tarifa) {
+        return tarifaService.actualizar(id, tarifa);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        tarifaService.eliminar(id);
+    }
 }
--- tpi-2025-grupo12-logistica/src/main/java/utn/frc/backend/tpi/logistica/controllers/TramoRutaController.java
+++ tpi-2025-grupo12-logistica/src/main/java/utn/frc/backend/tpi/logistica/controllers/TramoRutaController.java
@@ -1,5 +1,43 @@
 package utn.frc.backend.tpi.logistica.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import utn.frc.backend.tpi.logistica.models.TramoRuta;
+import utn.frc.backend.tpi.logistica.services.TramoRutaService;
+
+@RestController
+@RequestMapping("/api/logistica/tramos-ruta")
 public class TramoRutaController {
+
+    @Autowired
+    private TramoRutaService tramoRutaService;
+
+    @GetMapping
+    public List<TramoRuta> listar() {
+        return tramoRutaService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public TramoRuta listarPorId(@PathVariable Long id) {
+        return tramoRutaService.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public TramoRuta crear(@RequestBody TramoRuta tramoRuta) {
+        return tramoRutaService.crear(tramoRuta);
+    }
+
+    @PutMapping("/{id}")
+    public TramoRuta actualizar(@PathVariable Long id, @RequestBody TramoRuta tramoRuta) {
+        return tramoRutaService.actualizar(id, tramoRuta);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        tramoRutaService.eliminar(id);
+    }
 
 }
